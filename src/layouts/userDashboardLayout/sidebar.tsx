3a87aa58ee6f96b11cd740e9cbaff7598d06b39a
Joshua Oyewole <joshpp2013@gmail.com>
2022-11-09 12:25:44 +0100

    created the dashboardLayout for future usage

---
+++ src/layouts/userDashboardLayout/sidebar.tsx
@@ -0,0 +1,184 @@
+import DashboardStyles from "./_userDashboardLayout.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import SidebarNavItem from "../../components/ui/sidebarNavItem/sidebarNavItem";
+
+
+/* NAVLIST ICONS */
+/* Most of the Icons were not on FontAwesome thats why I'm using the images else icons are always prefferable */
+import usersIcon from "../../assets/images/user-friends.png";
+import guarantorIcon from "../../assets/images/users.png";
+import decisonModelIcon from "../../assets/images/handshake-regular.png";
+import loansIcon from "../../assets/images/sack.png";
+import savingsIcon from "../../assets/images/piggy-bank.png";
+import loanRequestIcon from "../../assets/images/loanRequest.png";
+import WhitelistIcon from "../../assets/images/user-check.png";
+import KarmaIcon from "../../assets/images/user-times.png";
+import oraganizationIcon from "../../assets/images/briefcase.png";
+import loanProductsIcon from "../../assets/images/loanRequest.png";
+import savingsProductsIcon from "../../assets/images/saving.png";
+import feesChargesIcon from "../../assets/images/coins-solid.png";
+import transactionsIcon from "../../assets/images/transactions.png";
+import servicesIcon from "../../assets/images/services.png";
+import serviceAccountIcon from "../../assets/images/servicesAccount.png";
+import settlementsIcon from "../../assets/images/settlement.png";
+import reportsIcon from "../../assets/images/reports.png";
+import preferencesIcon from "../../assets/images/preferences.png";
+import feesPricingIcon from "../../assets/images/feesPricing.png";
+import auditLogsIcon from "../../assets/images/auditLogs.png";
+import dashboardIcon from "../../assets/images/home.png";
+import switchOrganizationIcon from "../../assets/images/briefcase.png";
+
+
+type navListProps = {
+    icon: React.ReactNode,
+    text: string
+}
+
+
+const Sidebar = () => {
+    const icons = {
+        customer: [
+            {
+                icon: <img src={usersIcon} alt='usersIcon' />,
+                text: "Users"
+            },
+            {
+                icon: <img src={guarantorIcon} alt='guarantorIcon' />,
+                text: "Guarantors"
+            },
+            {
+                icon: <img src={loansIcon} alt='guarantorIcon' />,
+                text: "Loans"
+            },
+            {
+                icon: <img src={decisonModelIcon} alt='guarantorIcon' />,
+                text: "Decision Models"
+            },
+            {
+                icon: <img src={savingsIcon} alt='guarantorIcon' />,
+                text: "Savings"
+            },
+            {
+                icon: <img src={loanRequestIcon} alt='guarantorIcon' />,
+                text: "Loan Requests"
+            },
+            {
+                icon: <img src={WhitelistIcon} alt='guarantorIcon' />,
+                text: "Whitelist"
+            },
+            {
+                icon: <img src={KarmaIcon} alt='guarantorIcon' />,
+                text: "Karma"
+            },
+        ],
+        businesses: [
+            {
+                icon: <img src={oraganizationIcon} alt='guarantorIcon' />,
+                text: "Organization"
+            },
+            {
+                icon: <img src={loanProductsIcon} alt='guarantorIcon' />,
+                text: "Loan Products"
+            },
+            {
+                icon: <img src={savingsProductsIcon} alt='guarantorIcon' />,
+                text: "Savings Products"
+            },
+            {
+                icon: <img src={feesChargesIcon} alt='guarantorIcon' />,
+                text: "Fees and Charges"
+            },
+            {
+                icon: <img src={transactionsIcon} alt='guarantorIcon' />,
+                text: "Transactions"
+            },
+            {
+                icon: <img src={servicesIcon} alt='guarantorIcon' />,
+                text: "Services"
+            },
+            {
+                icon: <img src={serviceAccountIcon} alt='guarantorIcon' />,
+                text: "Service Account"
+            },
+            {
+                icon: <img src={settlementsIcon} alt='guarantorIcon' />,
+                text: "Settlements"
+            },
+            {
+                icon: <img src={reportsIcon} alt='guarantorIcon' />,
+                text: "Reports"
+            }
+        ],
+        settings: [
+            {
+                icon: <img src={preferencesIcon} alt='guarantorIcon' />,
+                text: "Preferences"
+            },
+            {
+                icon: <img src={feesPricingIcon} alt='guarantorIcon' />,
+                text: "Fees and Pricing"
+            },
+            {
+                icon: <img src={auditLogsIcon} alt='guarantorIcon' />,
+                text: "Audit Logs"
+            }
+        ]
+    }
+    return (
+        <aside className={DashboardStyles.dashboard__sidebar}>
+            <ul>
+                <SidebarNavItem navlink="#dashboard">
+                    <img src={switchOrganizationIcon} alt='dashboard icon' />
+                    <div className="mr-sIcon">
+                        <span> Switch Organization </span>
+                        <FontAwesomeIcon icon={faChevronDown} />
+                    </div>
+                </SidebarNavItem>
+
+                <SidebarNavItem navlink="#dashboard">
+                    <img src={dashboardIcon} alt='dashboard icon' /> <span>Dashboard</span>
+                </SidebarNavItem>
+            </ul>
+            <section className={DashboardStyles["dashboard__sidebar--section"]}>
+                <h4 className={DashboardStyles["dashboard__sidebar--sectionTitle"]}>CUSTOMERS</h4>
+                <ul>
+                    {
+                        icons.customer.map((list: navListProps, index: number) => {
+                            return <SidebarNavItem navlink="#dashboard" key={index}>
+                                {list.icon} <span>{list.text}</span>
+                            </SidebarNavItem>
+                        })
+                    }
+                </ul>
+            </section>
+            <section className={DashboardStyles["dashboard__sidebar--section"]}>
+                <h4 className={DashboardStyles["dashboard__sidebar--sectionTitle"]}>BUSINESSES</h4>
+                <ul>
+                    {
+                        icons.businesses.map((list: navListProps, index: number) => {
+                            return <SidebarNavItem navlink="#dashboard" key={index}>
+                                {list.icon} <span>{list.text}</span>
+                            </SidebarNavItem>
+                        })
+                    }
+                </ul>
+            </section>
+
+            <section className={DashboardStyles["dashboard__sidebar--section"]}>
+                <h4 className={DashboardStyles["dashboard__sidebar--sectionTitle"]}>SETTINGS</h4>
+                <ul>
+                    {
+                        icons.settings.map((list: navListProps, index: number) => {
+                            return <SidebarNavItem navlink="#dashboard" key={index}>
+                                {list.icon} <span>{list.text}</span>
+                            </SidebarNavItem>
+                        })
+                    }
+                </ul>
+            </section>
+        </aside>
+    )
+}
+
+export default Sidebar
--- src/layouts/userDashboardLayout/userDashboardLayout.tsx
+++ src/layouts/userDashboardLayout/userDashboardLayout.tsx
@@ -1,37 +1,72 @@
-import OnboardingStyles from "./_userDashboardLayout.module.scss"
+import DashboardStyles from "./_userDashboardLayout.module.scss"
 import logo from "../../assets/images/logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { faCaretDown, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Sidebar from "./sidebar";
 
 
-type OnboardingProps = {
+type DashboardProps = {
     children: React.ReactNode;
 }
 
 
-function UserDashboardLayout(props: OnboardingProps) {
 
+function UserDashboardLayout(props: DashboardProps) {
+    const { children } = props;
+
+    
     return (
-        <div className={OnboardingStyles.onboarding}>
-
-            {/* SIDE BAR */}
-            <div className={OnboardingStyles.onboarding__sidebar}>
-                <div className={OnboardingStyles.onboarding__logo}>
-                    <img src={logo} alt='brand logo' />
-                    <p className={OnboardingStyles['onboarding__logo--text']}>
-                        <span className={OnboardingStyles['onboarding__logo--text-fword']}>Accretio</span>
-                        <span className={OnboardingStyles['onboarding__logo--text-sword']}>Africa</span>
-                    </p>
+        <div className={DashboardStyles.dashboard__container}>
+            {/* HEADER SECTION (TOP NAVBAR) */}
+            <header className={DashboardStyles.dashboard__header}>
+                <div className={DashboardStyles.dashboard__logoContainer}>
+                    <img
+                        src={logo}
+                        alt="LendSqr Logo"
+                        className={DashboardStyles.dashboard__logo}
+                    />
                 </div>
-
-                {/* SIDE NAVBAR */}
-                <div className={OnboardingStyles.onboarding__sideNav}>
-
-                
+                <div className={DashboardStyles.dashboard__searchContainer}>
+                    <input
+                        type="search"
+                        name="search"
+                        placeholder="Search for anything"
+                        className={DashboardStyles.dashboard__searchInput}
+                    />
+                    <button className={DashboardStyles.dashboard__submitSearch}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
                 </div>
+                <div className={DashboardStyles.dashboard__headerNavWrapper}>
+                    <div className="linkContainer">
+                        <a href="#doc" className={DashboardStyles["dashboard__navlink--doc"]}>Docs</a>
+                    </div>
+                    <button className={DashboardStyles.userProfile__icon}>
+                        <FontAwesomeIcon icon={faBell} className={DashboardStyles.userProfile__bellIcon} />
+                    </button>
+                    <div className={DashboardStyles.userProfile__container}>
+                        <div className={DashboardStyles.userProfile__picture}></div>
+                        <h4 className={DashboardStyles.userProfile__userName}>Adedeji</h4>
+                        <button className={DashboardStyles.userProfile__icon}>
+                            <FontAwesomeIcon
+                                icon={faCaretDown}
+                            />
+                        </button>
+                    </div>
+                </div>
+            </header>
 
+            <div className={DashboardStyles.dashboard__wrapper}>
+                {/* SIDEBAR */}
+                <Sidebar />
+
+                {/* MAIN SECTION */}
+                <main className={DashboardStyles.dashboard__content}>
+                    {children}
+                </main>
             </div>
 
-            {/* ONBOARDING MODAL */}
-            {props.children}
         </div>
 
     )

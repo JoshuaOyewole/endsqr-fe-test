e2835797688bcd375d2c547b1876b8012c6da3f6
Joshua Oyewole <joshpp2013@gmail.com>
2022-11-10 23:11:11 +0100

    added a select field for filtering user details

---
+++ src/components/form/Select/select.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import GeneralInputStyles from "../_common-input-styles.module.scss";
+import SelectStyles from "./_select.module.scss";
+import ReactSelect from "react-select";
+import makeAnimated from "react-select/animated";
+
+const animatedComponents = makeAnimated();
+
+export interface SelectProps {
+  selectOptions: Array<any>;
+  selectPlaceholder?: string | number;
+  label?: string;
+  mutipleSelect?: boolean;
+  isRequired?: boolean;
+  [rest: string]: any;
+}
+
+const Select: React.FC<SelectProps> = (props, _ref) => {
+  const {
+    selectOptions,
+    selectPlaceholder,
+    label,
+    mutipleSelect,
+    isRequired,
+    ...rest
+  } = props;
+
+
+  return (
+    <div className={`xs ${GeneralInputStyles.input_container}`}>
+      {label && (
+        <label className="input-label">
+          {label}
+          {isRequired && <sup>*</sup>}
+        </label>
+      )}
+
+      <div className={`  ${GeneralInputStyles.input_field_container} `}>
+        <ReactSelect
+          className={`${SelectStyles.react_select_container}`}
+          classNamePrefix="react_select"
+          components={animatedComponents}
+          options={selectOptions}
+          placeholder={selectPlaceholder ? selectPlaceholder : "Select"}
+          isMulti={mutipleSelect}
+          closeMenuOnSelect={!mutipleSelect}
+          {...rest}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Select;

47f5677cd946dbe6e93ec90164d02fc54bac4a62
Joshua Oyewole <joshpp2013@gmail.com>
2022-11-11 11:01:08 +0100

    created user details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login/index";
 import Dashboard from "./pages/dashboard/index";
-import UserProfile from "./pages/user/index";
+import UserProfile from "./pages/users/index";
 import Error from "./pages/404/index";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="dashboard" element={<Dashboard />} />
       <Route path="dashboard/user" element={<UserProfile  />} />
+      <Route path="dashboard/user/:id" element={<UserProfile  />} />
       <Route path="*" element={<Error />} />
     </Routes>
   )
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,17 @@
+
+import UserDetails from "../../components/ui/userComponent/userDetail";
+import UserDashboardLayout from "../../layouts/userDashboardLayout/userDashboardLayout";
+
+type Props = {}
+
+export default function index({ }: Props) {
+
+
+  return (
+    <div className="wrapper">
+      <UserDashboardLayout>
+        <UserDetails />
+      </UserDashboardLayout>
+    </div>
+  )
+}

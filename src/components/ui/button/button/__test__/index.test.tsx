524f8990c700867c8e8516559ebdd9584db87f4e
Joshua Oyewole <joshpp2013@gmail.com>
2022-11-14 10:37:27 +0100

    debug the error affecting deployment

--- src/components/ui/button/button/__test__/index.test.tsx
+++ src/components/ui/button/button/__test__/index.test.tsx
@@ -1,7 +1,8 @@
+
 import ReactDOM from "react-dom";
-import Button from "../button
+import Button from "../button"
 import {render, cleanup,screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect"
+import '@testing-library/jest-dom'
 
 afterEach(cleanup)
 it("renders button without crashing", () => {
